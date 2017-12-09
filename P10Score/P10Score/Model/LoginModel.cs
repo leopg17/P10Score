f4235bbde2e883df172c00d4f0f08ea6485dfd1b
ARES\Leo <leopg17@gmail.com>
2017-12-09 11:57:10 -0600

    LoginModel

---
+++ P10Score/P10Score/Model/LoginModel.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10Score.Model
+{
+    class LoginModel
+    {
+
+        public async Task<string> Authenticate(string UserName, string Password)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var uri = new Uri("Aqui va la url de mi API");
+
+                var json = JsonConvert.SerializableObject(new
+
+                    UserNotifications = UserName,
+                    Password = PS.Security.Cryptography.GenerateSHA512String(Password)
+
+
+            });
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync(Uri, content).ConfigureAwait(false);
+            string ans = await response.Content.ReadAsStringAsync();
+        }
+               
+
+        
+            
+            
+    }
+}
---
+++ P10Score/P10Score/ViewModel/LoginViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace P10Score.ViewModel
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        #region Instances
+
+        public ICommand LoginCommand { get; set; }
+
+        private string _User { get; set; }
+
+        public string User
+        {
+            get
+            {
+                return _User;
+            }
+            set
+            {
+                _User = value;
+            }
+
+        }
+
+        private string _Password { get; set; }
+
+        public string Password
+        {
+            get
+            {
+                return _Password;
+            }
+            set
+            {
+                _Password = value;
+            }
+
+        }
+
+        #endregion
+        public LoginViewModel()
+        {
+            InitCommands();
+        }
+
+        #region Methods
+        private void Login()
+        {
+
+            if (LoginModel.Auntenticar(User, Password))
+            {
+                NavigationPage navigation = new NavigationPage(new HomePage());
+
+                App.Current.MainPage = new MasterDetailPage
+                {
+                    Master = new HomeMenu(),
+                    Detail = navigation
+                };
+            }
+            else
+            {
+                App.Current.MainPage.DisplayAlert("Mensaje", "Credenciales Invalidas", "OK");
+            }
+
+
+        }
+
+        private void InitCommands()
+        {
+            LoginCommand = new Command(Login);
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null) // if there is any subscribers 
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
