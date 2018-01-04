c98f6e57c4a37dfd4666ec1a380c2fdb88758df6
Leonardo Porras <leopg17@hotmail.com>
2018-01-04 09:46:13 -0600

    Implementacion de Iconos en el TabbedPage

--- P10Score/P10Score/View/HomePage.xaml.cs
+++ P10Score/P10Score/View/HomePage.xaml.cs
@@ -10,11 +10,11 @@ using Xamarin.Forms.Xaml;
 namespace P10Score.View
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class HomePage : ContentPage
+	public partial class HomePage : TabbedPage
 	{
 		public HomePage ()
 		{
 			InitializeComponent ();
-		}
+        }
 	}
 }
